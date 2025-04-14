ea272db571029bf22d5f03a3d381c22fcd800579
NaManiac91 <ge.costantino91@gmail.com>
2025-04-14 12:25:16 +0200

    added player logic + some refactor. + new farmer images.

--- scripts/game-ingine.mjs
+++ scripts/game-ingine.mjs
@@ -1,20 +1,40 @@
-const objects = [
+// Global variables
+const objects = [   // Objects to dig
     'seedling.svg',
     'wood.svg',
     'rock.svg'
-]
+];
+
+const points = document.getElementById('points');
+const playerName = document.getElementById('player-name');
+const gameOver = document.getElementById('game-over');
+const pRecord = document.getElementById('record');
+
+let hoeLevel = 1;
+let gridSize = 5;
+let level = 1;
+let timeLeft = 30; // seconds
+
+export function reset() {
+    gameOver.style.display = 'none';
+
+    hoeLevel = 1;
+    gridSize = 5;
+    level = 1;
+    timeLeft = 30; // seconds
+}
 
 // Create the grid in the DOM and fill the grid with random items
-export function initGrid(n) {
+export function initGrid() {
     const grid = document.createElement('div');
     grid.id = 'grid';
     let points = 0;
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < gridSize; i++) {
         let row = document.createElement('div');
         row.className = 'row';
         grid.append(row);
-        for (let j = 0; j < n; j++) {
+        for (let j = 0; j < gridSize; j++) {
             // Create cell
             let cell = document.createElement('div');
             cell.className = 'cell cell-' + i + '-' + j;
@@ -25,7 +45,7 @@ export function initGrid(n) {
             img.src = 'assets/' + objects[value];
             cell.appendChild(img);
             cell.setAttribute('value', value);
-            points += value + 1;
+            points += value + hoeLevel;
 
             // Append to the row
             row.append(cell);
@@ -36,7 +56,54 @@ export function initGrid(n) {
     return grid;
 }
 
-export function move(event, row, col, farmer, grid, n) {
+export function initPoints(points, leaderboard) {
+    // Init points and leaderboard
+    let record = JSON.parse(localStorage.getItem("Record"));
+
+    if (!record) {
+        record = {name: playerName.value, value: 0};
+        localStorage.setItem("Record", JSON.stringify(record));
+    }
+
+    points.setAttribute('value', 0);
+    points.setAttribute('current', 0);
+    points.innerText = 'Points: 0';
+
+    pRecord.id = 'record';
+    pRecord.innerText = `${record.name}: ${record.value}`;
+}
+
+function updateRecord() {
+    const record = JSON.parse(localStorage.getItem("Record"));
+    const currentScore = Number(points.getAttribute('value'));
+    if (currentScore > record.value) {
+        record.value = currentScore;
+        record.name = playerName.value;
+        localStorage.setItem("Record", JSON.stringify(record));
+        pRecord.innerText = `${record.name}: ${currentScore}`;
+    }
+    document.getElementById('grid').remove();
+    gameOver.style.display = 'block';
+}
+
+export function createTimer(timerDisplay) {
+    // Show timer
+    timerDisplay.style.display = 'block';
+    timerDisplay.textContent = timeLeft;
+
+    // Add interval to update seconds
+    const countdown = setInterval(() => {
+        timeLeft--;
+        timerDisplay.textContent = timeLeft;
+
+        if (timeLeft <= 0) {
+            clearInterval(countdown);
+            updateRecord();     // Update record if needed
+        }
+    }, 1000); // every 1000ms = 1 second
+}
+
+export function move(event, row, col, farmer, grid) {
     const currentPosition = grid.getElementsByClassName('cell-' + row + '-' + col)[0];
     currentPosition.children[0].style.display = 'block';    // show the object "behind" the farmer
 
@@ -56,7 +123,7 @@ export function move(event, row, col, farmer, grid, n) {
     }
 
     // Moving the farmer if the movement is allowed
-    if (col < n && col >= 0 && row >= 0 && row < n) {
+    if (col < gridSize && col >= 0 && row >= 0 && row < gridSize) {
         const newPosition = grid.getElementsByClassName('cell-' + row + '-' + col)[0];
         newPosition.children[0].style.display = 'none';
         newPosition.appendChild(farmer);
@@ -69,10 +136,10 @@ export function action(row, col, grid, points) {
     // Update the currentValue
     let currentValue = cell.getAttribute('value');
     if (currentValue >= 0) {
-        currentValue = currentValue - 1;
+        currentValue = currentValue - hoeLevel;
 
-        const value = Number(points.getAttribute('value')) + 1;
-        const current = Number(points.getAttribute('current')) + 1;
+        const value = Number(points.getAttribute('value')) + hoeLevel;
+        const current = Number(points.getAttribute('current')) + hoeLevel;
         points.setAttribute('value', value);
         points.setAttribute('current', current);
         points.innerText = `Points: ${value}`;
@@ -101,4 +168,15 @@ export function createFarmer(grid) {
     firstCell.appendChild(farmer);
 
     return farmer;
+}
+
+export function levelUp() {
+    if (gridSize < 10) {
+        gridSize++;     // update the size  of the grid
+    }
+    level++;            // update the level
+    if (level % 5 === 0) {
+        hoeLevel++;     // update the hoe level each 5 level
+    }
+    timeLeft += 30;     // add 30 extra seconds to the timer
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,65 +1,82 @@
-import {action, createFarmer, initGrid, move} from "./game-ingine.mjs";
-import {createTimer, initPoints} from "./ui-factory.mjs";
+import {action, createFarmer, initGrid, levelUp, move, createTimer, initPoints, reset} from "./game-ingine.mjs";
 
 // Init core items
-const container = document.getElementById('container');
-const timer = document.getElementById('timer');
-const points = document.getElementById('points');
-const leaderboard = document.getElementById('leaderboard');
-let gridSize = 5;
-let currentPoints = 0;
+const player = document.getElementById('player');
+const saveButton = document.getElementById('player-button');
+const playerName = document.getElementById('player-name');
+const restart = document.getElementById('restart');
 
-// Init UI
+playerName.addEventListener('input', () => {
+    saveButton.disabled = !playerName.value;
+});
+
+saveButton.addEventListener('click', () => {
+    init();
+    player.remove();
+});
+
+restart.addEventListener('click', () => {
+    reset();
+    init();
+});
+
+
+function init() {
+    const container = document.getElementById('container');
+    const timer = document.getElementById('timer');
+    const points = document.getElementById('points');
+    const leaderboard = document.getElementById('leaderboard');
+    // Init UI
 // Init points and leaderboard
-initPoints(points, leaderboard);
+    initPoints(points, leaderboard);
 
 // Init timer
-createTimer(timer);
+    createTimer(timer);
 
 // Init grid
-let grid = initGrid(gridSize);
-let gridScore = grid.getAttribute('points');
-container.appendChild(grid);
+    let grid = initGrid();
+    let gridScore = grid.getAttribute('points');
+    container.appendChild(grid);
 
 // Init farmer position
-let farmer = createFarmer(grid);
+    let farmer = createFarmer(grid);
 
 // Init events for the game engine
-document.addEventListener('keydown', (e) => {
-    e.preventDefault();
+    document.addEventListener('keydown', (e) => {
+        e.preventDefault();
 
-    const coordinates = farmer.parentElement.className.split('-');
-    let row = Number(coordinates[1]);
-    let col = Number(coordinates[2]);
+        const coordinates = farmer.parentElement.className.split('-');
+        let row = Number(coordinates[1]);
+        let col = Number(coordinates[2]);
 
-    if (['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'].includes(e.code)) {
-        move(e, row, col, farmer, grid, gridSize);
-    } else if (e.code === 'Space') {
-        farmer.src = 'assets/farmer2.svg';
-        action(row, col, grid, points, currentPoints);
-    }
-});
-
-function reloadGrid() {
-    points.setAttribute('current', 0);
-    container.removeChild(container.firstChild);
-    grid = initGrid(gridSize);
-    gridScore = grid.getAttribute('points');
-    container.appendChild(grid);
-    farmer = createFarmer(grid);
-}
-
-document.addEventListener('keyup', (e) => {
-    e.preventDefault();
-
-    if (e.code === 'Space') {
-        farmer.src = 'assets/farmer.svg';
-
-        if (points.getAttribute('current') === gridScore) {
-            // Reload the grid with a new level
-            if (gridSize < 10)
-                gridSize++;
-            reloadGrid();
+        if (['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'].includes(e.code)) {
+            move(e, row, col, farmer, grid);
+        } else if (e.code === 'Space') {
+            farmer.src = 'assets/farmer2.svg';
+            action(row, col, grid, points);
         }
+    });
+
+    function reloadGrid() {
+        points.setAttribute('current', 0);
+        container.removeChild(container.firstChild);
+        grid = initGrid();
+        gridScore = grid.getAttribute('points');
+        container.appendChild(grid);
+        farmer = createFarmer(grid);
     }
-});
+
+    document.addEventListener('keyup', (e) => {
+        e.preventDefault();
+
+        if (e.code === 'Space') {
+            farmer.src = 'assets/farmer.svg';
+
+            if (points.getAttribute('current') === gridScore) {
+                // Reload the grid with a new level
+                levelUp();
+                reloadGrid();
+            }
+        }
+    });
+}
